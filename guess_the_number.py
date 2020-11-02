07d7e0b3db1edaaaadcccbefdf9a0dc8b3244f92
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2020-11-02 16:55:55 +0300

    oop

--- guess_the_number.py
+++ guess_the_number.py
@@ -16,19 +16,19 @@ print('Try to guess the number!!')
 time.sleep(3)
 
 n=1
-print(f'Attempt{n}')
-member_number=int(input('Enter a number:'))
+while member_number!=number:
+    print(f'Attempt{n}')
+    member_number=int(input('Enter a number:'))
 
-if number==member_number:
-    print(f'Number is {number} You Win!!!')
-
-else:
-    while member_number!=number:
-        print(f'Attempt{n+1}')
-        member_number = int(input('Enter a number:'))
-        if member_number<number:
-            smoller(member_number)
+    if number==member_number:
+        if n<=6:
+            print(f'Number is {number}, You Win!!!')
         else:
-            bigger(member_number)
-        n=n+1
+            print(f'Number is {number}, You Lose!!!')
+
+    elif member_number>number:
+        smoller(member_number)
+    else:
+        bigger(member_number)
+    n=n+1
 
