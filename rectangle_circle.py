c0bcb805e06664ad5b4a5d39d9f3a96319ab1c29
vladimir <grun_gespenst@tut.by>
2020-11-07 23:01:02 +0300

    rectangle_def

--- rectangle_circle.py
+++ rectangle_circle.py
@@ -1,33 +1,35 @@
 from math import sqrt
 import pygame
 from pygame.draw import *
+def draw(hight, weght):
+    """Рисует окружность, описывающую прямоугольник"""
+    pygame.init()
+    FPS = 1
+    display_x=1000
+    display_y=1000
 
-pygame.init()
-FPS = 1
-display_x=1000
-display_y=1000
+    screen = pygame.display.set_mode((display_x, display_y))
 
-screen = pygame.display.set_mode((display_x, display_y))
+    begin_point_x=int(display_x/2)-int(weght/2)
+    begin_point_y=int(display_y/2)-int(hight/2)
 
-weght=660
-hight=500
+    diametr=int(sqrt(pow(weght, 2) + pow(hight, 2)))
 
-begin_point_x=int(display_x/2)-int(weght/2)
-begin_point_y=int(display_y/2)-int(hight/2)
+    rect(screen, (0, 0, 255), (begin_point_x, begin_point_y, weght, hight), 1)
+    circle(screen, (255, 255, 255), (int(display_x/2), int(display_y/2)), int(diametr/2), 1)
 
-diametr=int(sqrt(pow(weght, 2) + pow(hight, 2)))
+    pygame.display.update()
+    clock = pygame.time.Clock()
+    finished = False
 
-rect(screen, (0, 0, 255), (begin_point_x, begin_point_y, weght, hight))
-circle(screen, (255, 255, 255), (int(display_x/2), int(display_y/2)), int(diametr/2), 1)
+    while not finished:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
 
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
+    pygame.quit()
 
-while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-
-pygame.quit()
+hight=608
+weght=750
+draw(hight, weght)
