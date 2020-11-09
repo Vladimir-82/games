9417ec3a1677301c292d95714c24ed9233cb73b5
vladimir <grun_gespenst@tut.by>
2020-11-09 22:48:14 +0300

    smile.py

---
+++ add.py
@@ -0,0 +1,10 @@
+def add(object_1, object_2):
+    if type(object_1)==int and type(object_2)==int:
+        return str(object_1) + str(object_2)
+    elif type(object_1)==str and type(object_2)==str:
+        return int(object_1) + int(object_2)
+    else:
+        return None
+obj1=2
+obj2='3'
+print(add(obj1, obj2))
