78fadf1c4a0c92dbaa4c4f8b28a620673d6a85e4
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2020-11-12 16:50:42 +0300

    The hurt of the programm

--- oop_unit.py
+++ oop_unit.py
@@ -21,5 +21,16 @@ dic_goods=dict(zip(goods_list, price_list))
 print(dic_goods)
 
 contribution_from_other=float(sum(contributions_list))/count_of_members
+print(contributions_list)
 
+#тут должен быть код алгоритма распределения средств
 if sum(contributions_list)==sum(price_list):
+
+    for i, j in dic_members.items():
+        difference=abs(contribution_from_other-i)
+        if i> contribution_from_other:
+
+        elif i!=contribution_from_other:
+
+else:
+    print('You entered a false data')
