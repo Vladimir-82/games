32f9c126d00ff834766c948e90c16c111a2bd533
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2020-11-23 16:16:41 +0300

    23 11 20

--- new_file.py
+++ new_file.py
@@ -1,8 +1,11 @@
 from random import randint
-mass=[[randint(1,10) for i in range(1,4)] for j in range(1,4)]
+mass=[[randint(1,10) for i in range(3)] for j in range(3)]
 print(mass)
+index_list=[]
+number_list=[]
 for i in mass:
     for j in i:
         if j==3:
-            print(mass.index(j))
+            number_list.append(j)
 
+print(number_list)
