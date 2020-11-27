20a80ba9f7437b1264aaeb037b0e260db29bf2c5
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2020-11-27 14:39:47 +0300

    введите букву русского алфавита

--- hanger_my_version.py
+++ hanger_my_version.py
@@ -1,5 +1,26 @@
 import random
 
+def letter_being_allready():
+    letter_list = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+                   'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'э', 'ю', 'я']
+
+    current_letter=None
+    while current_letter not in letter_list:
+        current_letter = input('Введите букву русского алфавита:')
+    current_letter=current_letter.lower()
+    return current_letter
+    '''
+    named_letters=[]
+    if current_letter not in letter_list:
+        print('Введите букву!')
+        return -1
+    if current_letter not in named_letters:
+        named_letters.append(current_letter)
+        return current_letter
+    else:
+        print('Вы называли эту букву!')
+        return -1
+'''
 def display(HANGMAN_PICS, WORDS):
 
     choes = random.randint(0, len(WORDS) - 1)
@@ -10,11 +31,13 @@ def display(HANGMAN_PICS, WORDS):
     index_hangman = 0
 
     while '*' in show_current_word:
-        current_letter = input('Введите букву:')
-        current_letter=current_letter.lower()
+        #current_letter = input('Введите букву:')
+        #current_letter=current_letter.lower()
+        current_letter=letter_being_allready()
+
         if current_letter in current_word:
 
-            for index_letter in range(0, len(current_word)):
+            for index_letter in range(len(current_word)):
                 if current_word[index_letter]==current_letter:
                     show_current_word = show_current_word[0:index_letter:] + current_letter + show_current_word[index_letter + 1::]
                     print(show_current_word)
