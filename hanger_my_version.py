748702202ad93fe294e39562428f334a3401eb0c
vladimir <grun_gespenst@tut.by>
2020-11-28 11:58:35 +0300

    play again

--- hanger_my_version.py
+++ hanger_my_version.py
@@ -3,24 +3,11 @@ import random
 def letter_being_allready():
     letter_list = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
                    'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'э', 'ю', 'я']
-
     current_letter=None
-    while current_letter not in letter_list:
-        current_letter = input('Введите букву русского алфавита:')
-    current_letter=current_letter.lower()
+    while current_letter not in letter_list or len(current_letter)>1:
+        current_letter = input('Введите букву русского алфавита:').lower()
     return current_letter
-    '''
-    named_letters=[]
-    if current_letter not in letter_list:
-        print('Введите букву!')
-        return -1
-    if current_letter not in named_letters:
-        named_letters.append(current_letter)
-        return current_letter
-    else:
-        print('Вы называли эту букву!')
-        return -1
-'''
+
 def display(HANGMAN_PICS, WORDS):
 
     choes = random.randint(0, len(WORDS) - 1)
@@ -31,8 +18,6 @@ def display(HANGMAN_PICS, WORDS):
     index_hangman = 0
 
     while '*' in show_current_word:
-        #current_letter = input('Введите букву:')
-        #current_letter=current_letter.lower()
         current_letter=letter_being_allready()
 
         if current_letter in current_word:
@@ -46,6 +31,7 @@ def display(HANGMAN_PICS, WORDS):
 
         else:
             print((HANGMAN_PICS[index_hangman]))
+            print(show_current_word)
             if index_hangman==len(HANGMAN_PICS)-1:
                 print('YOU LOST')
                 break
@@ -93,6 +79,11 @@ HANGMAN_PICS=["""
 
 print('Начнем!')
 print()
-display(HANGMAN_PICS, WORDS)
-
-print()
+while True:
+    display(HANGMAN_PICS, WORDS)
+    game=input('Поиграем еще? (Да, Нет):').lower()
+    if game=='y' or game=='yes' or game=='д' or game=='да':
+        print('Ok!')
+    else:
+        print('Goodbuy!')
+        break
