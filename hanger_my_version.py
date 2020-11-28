663c0c654a2127ed75d113c6ebd796b646cde488
vladimir <grun_gespenst@tut.by>
2020-11-28 21:57:37 +0300

    repeat

--- hanger_my_version.py
+++ hanger_my_version.py
@@ -1,21 +1,28 @@
 import random
 
-def letter_being_allready():
+def letter_being_allready(): #еще не закончена
     letter_list = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
                    'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'э', 'ю', 'я']
+    letter_being_allready_list=[]
     current_letter=None
-    while current_letter not in letter_list or len(current_letter)>1:
-        current_letter = input('Введите букву русского алфавита:').lower()
-    return current_letter
+    while True:
+        if current_letter not in letter_list or len(current_letter)>1:
+            current_letter = input('Введите букву русского алфавита:').lower()
+            #letter_being_allready_list.append(current_letter)
+            #if current_letter in letter_being_allready_list:
+                #print('Вы уже называли эту букву!!!')
+        else:
+            return current_letter
 
 def display(HANGMAN_PICS, WORDS):
 
     choes = random.randint(0, len(WORDS) - 1)
     current_word = WORDS[choes]
     show_current_word = '*' * len(current_word)
+    print((HANGMAN_PICS[0]))
     print(show_current_word)
 
-    index_hangman = 0
+    index_hangman = 1
 
     while '*' in show_current_word:
         current_letter=letter_being_allready()
